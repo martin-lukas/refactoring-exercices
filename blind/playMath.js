638aa79ab6f75b54a30384bd3c25fa368dadf2b2
Martin Lukas <martinlukas24@protonmail.com>
2023-04-29 16:12:13 +0100

    Blind: refactor play type switch statements

--- blind/playMath.js
+++ blind/playMath.js
@@ -1,36 +1,45 @@
-function calculateTragedyPrice(audience) {
-  let sum = 40000;
-  if (audience > 30) {
-    sum += 1000 * (audience - 30);
-  }
-  return sum / 100;
-}
-
-function calculateComedyPrice(audience) {
-  let sum = 30000;
-  if (audience > 20) {
-    sum += 10000 + 500 * (audience - 20);
-  }
-  sum += 300 * audience;
-  return sum / 100;
-}
-
 export function calculatePlayPrice(playType, audience) {
-  switch (playType) {
-    case "tragedy":
-      return calculateTragedyPrice(audience);
-    case "comedy":
-      return calculateComedyPrice(audience);
-    default:
-      throw new Error(`unknown type: ${playType}`);
-  }
+    const typeConfig = getPlayTypeConfig(playType);
+    let totalPriceInCents = typeConfig.price + typeConfig.personMultiplier * audience;
+    if (audience > typeConfig.bonusThreshold) {
+        totalPriceInCents += typeConfig.baseBonus + typeConfig.bonusPersonMultiplier * (audience - typeConfig.bonusThreshold);
+    }
+    return totalPriceInCents / 100;
 }
 
 export function calculateVolumeCredits(playType, audience) {
-  let sum = Math.max(audience - 30, 0);
-  // add extra credit for every ten comedy attendees
-  if (playType === "comedy") {
-    sum += Math.floor(audience / 5);
-  }
-  return sum;
+    const typeConfig = getPlayTypeConfig(playType);
+    let credits = Math.max(audience - 30, 0);
+    // add extra credit for every X play attendees if configured for the play type
+    if (typeConfig.volumeCreditPer !== null) {
+        credits += Math.floor(audience / typeConfig.volumeCreditPer);
+    }
+    return credits;
+}
+
+function getPlayTypeConfig(playType) {
+    const typeConfig = PLAY_TYPES[playType];
+    if (typeConfig === undefined) {
+        throw new Error(`Unknown play type: ${playType}`);
+    }
+    return typeConfig;
+}
+
+const PLAY_TYPES = {
+    tragedy: {
+        price: 40000,
+        personMultiplier: 0,
+        bonusThreshold: 30,
+        baseBonus: 0,
+        bonusPersonMultiplier: 1000,
+        volumeCreditPer: null,
+    },
+    comedy: {
+        price: 30000,
+        personMultiplier: 300,
+        bonusThreshold: 20,
+        baseBonus: 10000,
+        bonusPersonMultiplier: 500,
+        volumeCreditPer: 5,
+    },
 }
