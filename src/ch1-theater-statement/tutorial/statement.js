afe1ec2f92de98424adec054c1d40ca594ef98e7
Martin Lukas <martinlukas24@protonmail.com>
2023-04-30 16:23:11 +0100

    Tutorial: make all supporting functions nested

--- src/ch1-theater-statement/tutorial/statement.js
+++ src/ch1-theater-statement/tutorial/statement.js
@@ -2,71 +2,69 @@ const plays = require('../mocks/plays');
 
 function statement(invoice) {
     let result = `Statement for ${invoice.customer}\n`;
-
     for (let perf of invoice.performances) {
         result += `  ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
     }
-
     result += `Amount owed is ${usd(totalAmount(invoice))}\n`;
     result += `You earned ${volumeCredits(invoice)} credits\n`;
     return result;
-}
 
-function amountFor(performance) {
-    let result = 0;
+    function amountFor(performance) {
+        let result = 0;
 
-    switch (playFor(performance).type) {
-        case "tragedy":
-            result = 40000;
-            if (performance.audience > 30) {
-                result += 1000 * (performance.audience - 30);
-            }
-            break;
-        case "comedy":
-            result = 30000;
-            if (performance.audience > 20) {
-                result += 10000 + 500 * (performance.audience - 20);
-            }
-            result += 300 * performance.audience;
-            break;
-        default:
-            throw new Error(`unknown type: ${play.type}`);
+        switch (playFor(performance).type) {
+            case "tragedy":
+                result = 40000;
+                if (performance.audience > 30) {
+                    result += 1000 * (performance.audience - 30);
+                }
+                break;
+            case "comedy":
+                result = 30000;
+                if (performance.audience > 20) {
+                    result += 10000 + 500 * (performance.audience - 20);
+                }
+                result += 300 * performance.audience;
+                break;
+            default:
+                throw new Error(`unknown type: ${play.type}`);
+        }
+        return result;
     }
-    return result;
-}
 
-function totalAmount(invoice) {
-    let totalAmount = 0;
-    for (let perf of invoice.performances) {
-        totalAmount += amountFor(perf);
+    function totalAmount(invoice) {
+        let totalAmount = 0;
+        for (let perf of invoice.performances) {
+            totalAmount += amountFor(perf);
+        }
+        return totalAmount / 100;
     }
-    return totalAmount / 100;
-}
 
-function volumeCredits(invoice) {
-    let result = 0;
-    for (let perf of invoice.performances) {
-        result += volumeCreditsFor(perf);
+    function volumeCredits(invoice) {
+        let result = 0;
+        for (let perf of invoice.performances) {
+            result += volumeCreditsFor(perf);
+        }
+        return result;
     }
-    return result;
-}
 
-function volumeCreditsFor(performance) {
-    let result = 0
-    result += Math.max(performance.audience - 30, 0);
-    if ("comedy" === playFor(performance).type) result += Math.floor(performance.audience / 5);
-    return result;
-}
+    function volumeCreditsFor(performance) {
+        let result = 0
+        result += Math.max(performance.audience - 30, 0);
+        if ("comedy" === playFor(performance).type) result += Math.floor(performance.audience / 5);
+        return result;
+    }
 
-function playFor(performance) {
-    return plays[performance.playID];
-}
+    function playFor(performance) {
+        return plays[performance.playID];
+    }
 
-function usd(amount) {
-    return new Intl.NumberFormat("en-US", {
-        style: "currency", currency: "USD",
-        minimumFractionDigits: 2
-    }).format(amount);
+    function usd(amount) {
+        return new Intl.NumberFormat("en-US", {
+            style: "currency", currency: "USD",
+            minimumFractionDigits: 2
+        }).format(amount);
+    }
 }
 
 module.exports = statement;
