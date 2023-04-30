e541a936018ba791cd21e4a94d5a1e5125732385
Martin Lukas <martinlukas24@protonmail.com>
2023-04-30 11:25:17 +0100

    Tutorial: reorder functions

--- src/ch1-theater-statement/tutorial/statement.js
+++ src/ch1-theater-statement/tutorial/statement.js
@@ -1,32 +1,5 @@
 const plays = require('../mocks/plays');
 
-function forPlay(perf) {
-    return plays[perf.playID];
-}
-
-function amountFor(play, performance) {
-    let result = 0;
-
-    switch (play.type) {
-        case "tragedy":
-            result = 40000;
-            if (performance.audience > 30) {
-                result += 1000 * (performance.audience - 30);
-            }
-            break;
-        case "comedy":
-            result = 30000;
-            if (performance.audience > 20) {
-                result += 10000 + 500 * (performance.audience - 20);
-            }
-            result += 300 * performance.audience;
-            break;
-        default:
-            throw new Error(`unknown type: ${play.type}`);
-    }
-    return result;
-}
-
 function statement(invoice) {
     let totalAmount = 0;
     let volumeCredits = 0;
@@ -52,6 +25,33 @@ function statement(invoice) {
     result += `Amount owed is ${format(totalAmount / 100)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
     return result;
+}
+
+function amountFor(play, performance) {
+    let result = 0;
+
+    switch (play.type) {
+        case "tragedy":
+            result = 40000;
+            if (performance.audience > 30) {
+                result += 1000 * (performance.audience - 30);
+            }
+            break;
+        case "comedy":
+            result = 30000;
+            if (performance.audience > 20) {
+                result += 10000 + 500 * (performance.audience - 20);
+            }
+            result += 300 * performance.audience;
+            break;
+        default:
+            throw new Error(`unknown type: ${play.type}`);
+    }
+    return result;
+}
+
+function forPlay(perf) {
+    return plays[perf.playID];
 }
 
 module.exports = statement;
