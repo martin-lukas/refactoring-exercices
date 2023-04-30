c155fa3b61875b73f8605ed9ae608267b6be95e3
Martin Lukas <martinlukas24@protonmail.com>
2023-04-30 11:48:34 +0100

    Tutorial: rename format function

--- src/ch1-theater-statement/tutorial/statement.js
+++ src/ch1-theater-statement/tutorial/statement.js
@@ -10,10 +10,10 @@ function statement(invoice) {
         volumeCredits += volumeCreditsFor(perf);
 
         // print line for this order
-        result += `  ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
+        result += `  ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
         totalAmount += amountFor(perf);
     }
-    result += `Amount owed is ${format(totalAmount / 100)}\n`;
+    result += `Amount owed is ${usd(totalAmount / 100)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
     return result;
 }
@@ -52,7 +52,7 @@ function playFor(performance) {
     return plays[performance.playID];
 }
 
-function format(amount) {
+function usd(amount) {
     return new Intl.NumberFormat("en-US", {
         style: "currency", currency: "USD",
         minimumFractionDigits: 2
