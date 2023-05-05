fba0cc41c5cab9a2a2ac7895ec7335a0293dfe17
Martin Lukas <martinlukas24@protonmail.com>
2023-05-05 01:08:25 +0100

    Tutorial: finish html test

--- src/ch1-theater-statement/tutorial/statement.test.js
+++ src/ch1-theater-statement/tutorial/statement.test.js
@@ -37,4 +37,12 @@ test('HTML Statement should output the correct HTML code on default inputs', () 
     const outputLines = outputStatement.split('\n');
     expect(outputLines[0]).toBe('<h1>Statement for Customer A</h1>');
     expect(outputLines[1]).toBe('<table>');
+    expect(outputLines[2]).toBe('<tr><th>play</th><th>seats</th><th>costs</th></tr>');
+    expect(outputLines[3]).toBe('<tr><td>Some Comedy</td><td>55</td><td>$740.00</td></tr>');
+    expect(outputLines[4]).toBe('<tr><td>Some Tragedy</td><td>35</td><td>$450.00</td></tr>');
+    expect(outputLines[5]).toBe('<tr><td>Another Comedy</td><td>40</td><td>$620.00</td></tr>');
+    expect(outputLines[6]).toBe('</table>');
+    expect(outputLines[7]).toBe('<p>Amount owed is <em>$1,810.00</em></p>');
+    expect(outputLines[8]).toBe('<p>You earned <em>59</em> credits</p>');
+    expect(outputLines[9]).toBe('');
 });
